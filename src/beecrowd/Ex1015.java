239f92f40b7f58b73af41179ffd3324047227980
Claudio Roberto Silva Fabiano <1076313@senacminas.edu.br>
2026-07-08 19:53:17 -0300

    atualizando 1015

--- src/beecrowd/Ex1015.java
+++ src/beecrowd/Ex1015.java
@@ -1,6 +1,25 @@
 package beecrowd;
 import java.io.IOException;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 public class Ex1015 {
-    
+    public static void main(String[] args) throws IOException {
+        
+        DecimalFormat df4 = new DecimalFormat("0.0000");
+        
+        Scanner scan = new Scanner(System.in);  
+        
+        double x1 = scan.nextDouble();
+        double y1 = scan.nextDouble();
+        
+        double x2 = scan.nextDouble();
+        double y2 = scan.nextDouble();
+
+        //Math.sqrt = calcula raiz quadrada
+        //Math.pow = calcula potencia
+        
+        double distancia = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        System.out.println(df4.format(distancia));
+    }
 }
